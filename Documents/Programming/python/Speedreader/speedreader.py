20ce6f005125d018fba5cd0b5105f8893c1e33da
Nick <nFernan1@uci.edu>
2015-04-25 21:35:00 -0700

    updated some test code

--- Documents/Programming/python/Speedreader/speedreader.py
+++ Documents/Programming/python/Speedreader/speedreader.py
@@ -1,20 +1,34 @@
 #Speedreader program
+#TODO LIST:
+    #Print a certain amount of words specified by user
+    #Make a gui
+    #make previous words disappear 
+
 #Scan
 import time
 
+fileToScan = 'inputText1.txt' #input("Scan File: ")
+speedOfPrint = 1.0 #float(input("Enter word speed: "))
+amountOfWords = 3 #int(input("Enter amount of words: "))
 
-fileToScan = input("Scan File: ")
-speedOfPrint = float(input("Enter word speed: "))
 print(fileToScan) # REMOVE
 
 # Prints word by word in the scanned file
 with open(fileToScan,'r') as readFile:
-    for line in readFile:
-        for word in line.split():
-            print(word)
+    wordList = [ word for line in readFile for word in line.split()]          
+        
 
-            # Delays the time a word will appear
-            time.sleep(speedOfPrint)
+position = 0
+for i in range(position, amountOfWords):
+    position = i
+    print(wordList[i], end=" ")
+    #print_word(position, endPosition)
+
+    # Delays the time a word will appear
+    # time.sleep(speedOfPrint)
+    
+print(position) #REMOVE
+print(wordList) #REMOVE
 
 
-hello this is an edit
+
