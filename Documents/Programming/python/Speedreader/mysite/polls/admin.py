4c18d0b83f363d6aa2f7f6206d907bc8ada8ce37
Nick <nFernan1@uci.edu>
2015-11-12 00:21:34 -0800

    updating the django code

--- Documents/Programming/python/Speedreader/mysite/polls/admin.py
+++ Documents/Programming/python/Speedreader/mysite/polls/admin.py
@@ -8,6 +8,8 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
 	list_display = ('question_text', 'pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 	inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
